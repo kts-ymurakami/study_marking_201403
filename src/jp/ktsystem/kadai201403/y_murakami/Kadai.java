30bea21c02cefb597a18a0071935b25ac5488d8e
y_murakami <kts.ymurakami@gmail.com>
2014-06-25 23:45:57 +0900

    #17 Lv1とLv2で別のクラスを作成

--- src/jp/ktsystem/kadai201403/y_murakami/Kadai.java
+++ src/jp/ktsystem/kadai201403/y_murakami/Kadai.java
@@ -5,82 +5,82 @@ import jp.ktsystem.kadai201403.y_murakami.common.KadaiException;
 import jp.ktsystem.kadai201403.y_murakami.util.ErrorUtil;
 
 /**
- * �ۑ�N���X
+ * �ｿｽﾛ托ｿｽN�ｿｽ�ｿｽ�ｿｽX
  *
  * @author y_murakami
  */
 public class Kadai {
 
 	/**
-	 * Lv1 ���t�A�J�n���ԁA�I�����Ԃ�Json�`���ŋL�q���ꂽ�t�@�C����ǂݍ��݁A �Ζ����Ԃ��t�@�C���ɏo�͂���
+	 * Lv1 �ｿｽ�ｿｽ�ｿｽt�ｿｽA�ｿｽJ�ｿｽn�ｿｽ�ｿｽ�ｿｽﾔ、�ｿｽI�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾔゑｿｽJson�ｿｽ`�ｿｽ�ｿｽ�ｿｽﾅ記�ｿｽq�ｿｽ�ｿｽ�ｿｽ黷ｽ�ｿｽt�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽ�ｿｽﾇみ搾ｿｽ�ｿｽﾝ、 �ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽﾔゑｿｽ�ｿｽt�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾉ出�ｿｽﾍゑｿｽ�ｿｽ�ｿｽ
 	 *
 	 * @param anInputPath
-	 *            ���̓t�@�C���p�X
+	 *            �ｿｽ�ｿｽ�ｿｽﾍフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽp�ｿｽX
 	 * @param anOutputPath
-	 *            �o�̓t�@�C���p�X
+	 *            �ｿｽo�ｿｽﾍフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽp�ｿｽX
 	 * @throws KadaiException
 	 */
 	public static void parseWorkTimeData(String anInputPath, String anOutputPath)
 			throws KadaiException {
 
-		// �Ζ����ԃt�@�C���ǂݏ�������I�u�W�F�N�g�̐���
+		// �ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽﾔフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾇみ擾ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽI�ｿｽu�ｿｽW�ｿｽF�ｿｽN�ｿｽg�ｿｽﾌ撰ｿｽ�ｿｽ�ｿｽ
 		WorkTimeFileIOControl workTimeFile = new WorkTimeFileIOControl(
 				anInputPath, anOutputPath);
 
-		// �t�@�C���ǂݍ���
+		// �ｿｽt�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾇみ搾ｿｽ�ｿｽ�ｿｽ
 		try {
 			workTimeFile.readInputFile();
 		} catch (KadaiException ex) {
 			int eCode = ex.getErrorCode();
-			// �o�̓t�@�C���̍쐬���K�v�Ȃ��G���[�R�[�h�̏ꍇ
+			// �ｿｽo�ｿｽﾍフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾌ作成�ｿｽ�ｿｽ�ｿｽK�ｿｽv�ｿｽﾈゑｿｽ�ｿｽG�ｿｽ�ｿｽ�ｿｽ[�ｿｽR�ｿｽ[�ｿｽh�ｿｽﾌ場合
 			if (ErrorCode.NULL_INPUT_FILE_PATH.getErrorCode() == eCode
 					|| ErrorCode.NOT_EXIST_INPUT_FILE.getErrorCode() == eCode
 					|| ErrorCode.FAILE_READ_INPUT_FILE.getErrorCode() == eCode) {
 				throw ex;
 			}
-			// �ǂݍ��߂Ă��镪���������o��(�ǂݍ��ݒ��̃G���[�̏ꍇ�̓G���[�R�[�h�o��)
+			// �ｿｽﾇみ搾ｿｽ�ｿｽﾟてゑｿｽ�ｿｽ髟ｪ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽo�ｿｽ�ｿｽ(�ｿｽﾇみ搾ｿｽ�ｿｽﾝ抵ｿｽ�ｿｽﾌエ�ｿｽ�ｿｽ�ｿｽ[�ｿｽﾌ場合�ｿｽﾍエ�ｿｽ�ｿｽ�ｿｽ[�ｿｽR�ｿｽ[�ｿｽh�ｿｽo�ｿｽ�ｿｽ)
 			workTimeFile.writeOutputFile(eCode);
-			return;// ����n�Ƃ��ďI��
+			return;// �ｿｽ�ｿｽ�ｿｽ�ｿｽn�ｿｽﾆゑｿｽ�ｿｽﾄ終�ｿｽ�ｿｽ
 		}
 
-		// ���̓t�@�C���ǂݍ��݂��S�ď�肭�������ꍇ�����o��
+		// �ｿｽ�ｿｽ�ｿｽﾍフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾇみ搾ｿｽ�ｿｽﾝゑｿｽ�ｿｽS�ｿｽﾄ擾ｿｽ閧ｭ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ鼾�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽo�ｿｽ�ｿｽ
 		workTimeFile.writeOutputFile(-1);
 
 	}
 
 	/**
-	 * Lv2 ���A���t�A�J�n���ԁA�I�����Ԃ�Json�`���ŋL�q���ꂽ�t�@�C����ǂݍ��݁A �Ζ����Ԃ��t�@�C���ɏo�͂���
+	 * Lv2 �ｿｽ�ｿｽ�ｿｽA�ｿｽ�ｿｽ�ｿｽt�ｿｽA�ｿｽJ�ｿｽn�ｿｽ�ｿｽ�ｿｽﾔ、�ｿｽI�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾔゑｿｽJson�ｿｽ`�ｿｽ�ｿｽ�ｿｽﾅ記�ｿｽq�ｿｽ�ｿｽ�ｿｽ黷ｽ�ｿｽt�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽ�ｿｽﾇみ搾ｿｽ�ｿｽﾝ、 �ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽﾔゑｿｽ�ｿｽt�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾉ出�ｿｽﾍゑｿｽ�ｿｽ�ｿｽ
 	 * @param anInputPath
-	 *            ���̓t�@�C���p�X
+	 *            �ｿｽ�ｿｽ�ｿｽﾍフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽp�ｿｽX
 	 * @param anOutputPath
-	 *            �o�̓f�B���N�g���p�X
+	 *            �ｿｽo�ｿｽﾍデ�ｿｽB�ｿｽ�ｿｽ�ｿｽN�ｿｽg�ｿｽ�ｿｽ�ｿｽp�ｿｽX
 	 * @throws KadaiException
 	 */
 	public static void parseWorkTimeDataLv2(String anInputPath,
 			String anOutputPath) throws KadaiException {
 
-		// �Ζ����ԃt�@�C���ǂݏ�������I�u�W�F�N�g�̐���
-		WorkTimeFileIOControl workTimeFile = new WorkTimeFileIOControl(
+		// �ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽﾔフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾇみ擾ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽI�ｿｽu�ｿｽW�ｿｽF�ｿｽN�ｿｽg�ｿｽﾌ撰ｿｽ�ｿｽ�ｿｽ
+		WorkTimeFileIOControlLv2 workTimeFile = new WorkTimeFileIOControlLv2(
 				anInputPath, anOutputPath);
 
 		boolean controlErrFlag = false;
-		// �t�@�C���ǂݍ���
+		// �ｿｽt�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾇみ搾ｿｽ�ｿｽ�ｿｽ
 		try {
-			// �t�@�C���̓ǂݍ���
+			// �ｿｽt�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾌ読み搾ｿｽ�ｿｽ�ｿｽ
 			workTimeFile.readInputFileLv2();
 		} catch (KadaiException ex) {
-			// �o�̓t�@�C���̍쐬���K�v�Ȃ��G���[�R�[�h�̏ꍇ
+			// �ｿｽo�ｿｽﾍフ�ｿｽ@�ｿｽC�ｿｽ�ｿｽ�ｿｽﾌ作成�ｿｽ�ｿｽ�ｿｽK�ｿｽv�ｿｽﾈゑｿｽ�ｿｽG�ｿｽ�ｿｽ�ｿｽ[�ｿｽR�ｿｽ[�ｿｽh�ｿｽﾌ場合
 			if (ErrorCode.NULL_INPUT_FILE_PATH.getErrorCode() == ex.getErrorCode()
 					|| ErrorCode.NOT_EXIST_INPUT_FILE.getErrorCode() == ex.getErrorCode()
 					|| ErrorCode.FAILE_READ_INPUT_FILE.getErrorCode() == ex.getErrorCode()) {
 				throw ex;
 			}
-			// ���䕶�����������Ă����ꍇ
+			// �ｿｽ�ｿｽ�ｿｽ苺ｶ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾄゑｿｽ�ｿｽ�ｿｽ�ｿｽ鼾�
 			if(ErrorCode.IS_CTRL_CODE.getErrorCode() == ex.getErrorCode()){
 				controlErrFlag = true;
 			}
 		}
-		// �����o��
+		// �ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽo�ｿｽ�ｿｽ
 		workTimeFile.writeOutPutFileLv2(anOutputPath);
 
 		if(controlErrFlag){
@@ -89,29 +89,29 @@ public class Kadai {
 	}
 
 	/**
-	 * �o�Ў��ԂƑގЎ��Ԃ���A�Ζ����Ԃ�Ԃ����\�b�h
+	 * �ｿｽo�ｿｽﾐ趣ｿｽ�ｿｽﾔと退社趣ｿｽ�ｿｽﾔゑｿｽ�ｿｽ�ｿｽA�ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽﾔゑｿｽﾔゑｿｽ�ｿｽ�ｿｽ�ｿｽ\�ｿｽb�ｿｽh
 	 *
 	 * @param aStartTime
-	 *            �o�Ў��ԁ@HHmm
+	 *            �ｿｽo�ｿｽﾐ趣ｿｽ�ｿｽﾔ　HHmm
 	 * @param anEndTime
-	 *            �ގЎ��� HHmm
-	 * @return �Ζ�����
+	 *            �ｿｽﾞ社趣ｿｽ�ｿｽ�ｿｽ HHmm
+	 * @return �ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ
 	 * @throws KadaiException
 	 */
 	public static String calcWorkTime(String aStartTime, String anEndTime)
 			throws KadaiException {
 
-		// ���̓G���[�`�F�b�N
+		// �ｿｽ�ｿｽ�ｿｽﾍエ�ｿｽ�ｿｽ�ｿｽ[�ｿｽ`�ｿｽF�ｿｽb�ｿｽN
 		ErrorUtil.checkStrTime(aStartTime, anEndTime);
 
-		// ���ԃN���X�̐���
+		// �ｿｽ�ｿｽ�ｿｽﾔク�ｿｽ�ｿｽ�ｿｽX�ｿｽﾌ撰ｿｽ�ｿｽ�ｿｽ
 		KadaiTime startTime = new KadaiTime(aStartTime);
 		KadaiTime endTime = new KadaiTime(anEndTime);
 
-		// �Ζ����Ԑ���N���X�̐���
+		// �ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽﾔ撰ｿｽ�ｿｽ�ｿｽN�ｿｽ�ｿｽ�ｿｽX�ｿｽﾌ撰ｿｽ�ｿｽ�ｿｽ
 		WorkTimeControl workTimeCtrl = new WorkTimeControl(startTime, endTime);
 
-		// �Ζ����Ԃ��擾
+		// �ｿｽﾎ厄ｿｽ�ｿｽ�ｿｽ�ｿｽﾔゑｿｽ�ｿｽ謫ｾ
 		return workTimeCtrl.returnWorkTime();
 
 	}
